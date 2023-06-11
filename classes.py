62ee9203152d5ee4e481482be9b69d1eda257f43
moein <moeinrezaie1382mrg@gmail.com>
2023-06-11 18:40:49 +0330

    add all info into one file

--- classes.py
+++ classes.py
@@ -20,10 +20,10 @@ class info():
         return general_information
 
 class courses():
-    def __init__(self, math_grd, phisics_grd, history_grd, computer_science_grd):
+    def __init__(self, math_grd, physics_grd, history_grd, computer_science_grd):
 
         self.math_grd = math_grd
-        self.phisics_grd = phisics_grd
+        self.physics_grd = physics_grd
         self.history_grd = history_grd
         self.computer_science_grd = computer_science_grd
     
@@ -31,7 +31,7 @@ class courses():
 
         courses_information = {
             "math grade" : self.math_grd,
-            "phisics grade" : self.phisics_grd,
+            "physics grade" : self.physics_grd,
             "history grade" : self.history_grd,
             "computer science grade" : self.computer_science_grd
         }
--- main.py
+++ main.py
@@ -5,9 +5,8 @@ import json
 def get_operation():
     global operation
     operation = int(
-        input(
-            "Enter you considered operation. [1]>Add Student, [2]>Search Student: "
-        ))
+        input("Enter you considered operation. [1]>Add Student, [2]>Search Student: ")
+    )
     if operation == 1 or operation == 2:
         pass
     else:
@@ -17,56 +16,67 @@ def get_operation():
 get_operation()
 
 if operation == 1:
-    #get info
+    # get info
     name = input("Enter Student's name: ")
     age = input("Enter Student's age: ")
     nationalid = input("Enter Student's NationalID: ")
     studentid = input("Enter Sudent's StudentID: ")
     edu = input("Enter Student's Edu year: ")
     student_ginfo = info(name, age, nationalid, studentid, edu)
-    #get grades
+    # get grades
     mathgrd = input("Enter Student's math grade: ")
-    phisicgrd = input("Enter Student's phisic grade: ")
+    physicgrd = input("Enter Student's physic grade: ")
     historygrd = input("Enter Student's history grade: ")
     csgrd = input("Enter Student's computer science grade: ")
-    student_cinfo = courses(mathgrd, phisicgrd, historygrd, csgrd)
-    #merge informations
+    student_cinfo = courses(mathgrd, physicgrd, historygrd, csgrd)
+    # merge informations
     all_info = {}
     all_info.update(student_ginfo.infos())
     all_info.update(student_cinfo.infos())
-    #make file and save data
+    # make file and save data
     try:
-        user_file = open(
-            "/Volumes/Files/Projects/Python-oop-database/data/database.txt",
-            "w")
-        user_file.write(json.dumps(all_info))
+        try:
+            user_file = open(
+                "/Volumes/Files/Projects/Python-oop-database/data/database.txt", "a"
+            )
+        except:
+            user_file = open(
+                "/Volumes/Files/Projects/Python-oop-database/data/database.txt", "w"
+            )
+
+        user_file.write(f"\n{json.dumps(all_info)}")
         user_file.close()
         print("Student added successfuly!")
     except:
         print("Couldn't add student")
 else:
-    #search person
+    # search person
     studentid = input("Enter the StudentID to show info: ")
     try:
         database = open(
-            "/Volumes/Files/Projects/Python-oop-database/data/database.txt",
-            "r+")
+            "/Volumes/Files/Projects/Python-oop-database/data/database.txt", "r+"
+        )
+
         all_students = database.readlines()
+        for line in all_students:
+            if len(line)<10:
+                all_students.remove(line)
+
         for std in range(len(all_students)):
             student = json.loads(all_students[std])
             if student["StudentID"] == studentid:
                 report = {
                     "name: ": student["name"],
                     "math grade: ": student["math grade"],
-                    "phisic grade: ": student["phisics grade"],
+                    "physic grade: ": student["physics grade"],
                     "history grade: ": student["history grade"],
-                    "computer science grade: ":
-                    student["computer science grade"]
+                    "computer science grade: ": student["computer science grade"],
                 }
 
         student_report = open(
             f"/Volumes/Files/Projects/Python-oop-database/gradereport/{studentid}.txt",
-            "w")
+            "w",
+        )
         student_report.write(json.dumps(report))
         student_report.close()
         database.close()
@@ -75,4 +85,4 @@ else:
     except:
         print("Student not found!")
 
-#Developed By Moein Rezaie
+# Developed By Moein Rezaie
